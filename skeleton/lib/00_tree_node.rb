2913277085d8fbcce79a176e061023f31744e629
J Resilient <j.resilient.1@gmail.com>
2019-10-09 14:12:14 -0400

    Add and test #initialize

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,14 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(el)
+        @parent = el
+        @children << parent if !parent.nil?
+    end
 end
